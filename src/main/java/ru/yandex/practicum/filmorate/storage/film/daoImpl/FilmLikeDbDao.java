529596b015877455e0b5805b30487a85630cf8aa
Nik <MescheryakovN@yandex.ru>
2023-03-08 11:50:52 +0300

    Sprint_12_add-search_Сборка_1.0

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/daoImpl/FilmLikeDbDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/daoImpl/FilmLikeDbDao.java
@@ -36,20 +36,17 @@ public class FilmLikeDbDao implements FilmLikeDao {
     //добавить лайки фильмам в таблицу films_like
     @Override
     public void addLike(long filmId, long userId) {
-        try{
-            String addSql="insert into  FILMS_LIKE (film_id, user_id) select ?, ? from dual where not exists "+
-                    "(select 1 from FILMS_LIKE where film_id=? AND user_id=?);";
-            Object[] args = new Object[] {filmId,userId,filmId,userId};
-            int addRow=jdbcTemplate.update(addSql, args);
-            if (addRow<=0) {
-                log.debug("Ошибка добавления для фильма с id={} лайка от пользователя с id={}.",filmId,userId);
-                throw new FilmNotFoundException("Фильм с id="+filmId+" или пользователь с id="+userId+" не найден.");
+        String updateSql = "UPDATE films_like SET film_id = ?, user_id = ? WHERE film_id = ? AND user_id = ?";
+        int updateRow=jdbcTemplate.update(updateSql, filmId, userId, filmId, userId);
+        if (updateRow == 0) {
+            String addSql = "INSERT INTO films_like (film_id, user_id) VALUES(?,?)";
+            int addRow = jdbcTemplate.update(addSql, filmId, userId);
+            if (addRow <= 0) {
+                log.debug("Ошибка добавления для фильма с id={} лайка от пользователя с id={}.", filmId, userId);
+                throw new FilmNotFoundException("Фильм с id=" + filmId + " или пользователь с id=" + userId + " не найден.");
             }
-        } catch (RuntimeException e) {
-            log.debug("Возникло исключение: фильм или пользователь не найдены.");
-            throw new FilmNotFoundException("Фильм с id="+filmId+" или пользователь с id="+userId+" не найден.");
         }
-        log.debug("Для фильма с id={} добавлен лайк пользователем с id={}.",filmId,userId);
+        log.debug("Для фильма с id={} добавлен лайк пользователем с id={}.", filmId, userId);
     }
 
     //удалить лайки фильмам из таблицы films_like
